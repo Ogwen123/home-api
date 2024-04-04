3639f765e32d293d562c431769569046659ace54
ogwen123 <owen.d.jones@btinternet.com>
2024-04-04 18:45:44 +0100

    things

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import bodyParser from "body-parser"
 import cors from "cors"
 import projectInfo from "./routes/projectInfo.ts"
+import { prisma } from "./utils/db.ts"
+import { error } from "./utils/api.ts"
 
 dotenv.config()
 
@@ -29,9 +31,35 @@ app.use('/*', function (req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => {
+app.use("/api/*", async (req, res, next) => {
+    const enabled = (await prisma.services.findUnique({
+        where: {
+            id: "abc3d324-9055-4cb5-8c3e-34a3da32b847"
+        },
+        select: {
+            enabled: true
+        }
+    }))?.enabled
+
+    if (enabled) {
+        next();
+    } else {
+        error(res, 403, "This service is disabled.")
+    }
+});
+
+app.get('/', async (req, res) => {
+    const enabled = (await prisma.services.findUnique({
+        where: {
+            id: "abc3d324-9055-4cb5-8c3e-34a3da32b847"
+        },
+        select: {
+            enabled: true
+        }
+    }))?.enabled
+
     res.send({
-        "message": "API is running"
+        "message": (enabled ? "API is running!" : "API is disabled!")
     })
 })
 
